15ef2bcc4effc5b7300bd08bf4ae44fb3165a538
RomanGrabovyi <60966944+RomanGrabovyi@users.noreply.github.com>
2022-12-21 22:34:35 +0200

    add applyMiddleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,36 @@
-import { createStore, combineReducers , compose} from 'redux';
+import { createStore, combineReducers , compose, applyMiddleware} from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const enhancer = (createStore) => (...arg) => {
-    const store = createStore(...arg);
-
-    const oldDispatch = store.dispatch;
-    store.dispatch = (action) => {
-        if (typeof action === 'string') {
-            return oldDispatch({
-                type: action
-            })
-        }
-        return oldDispatch(action);
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({
+            type: action
+        })
     }
-    return store;
+    return next(action)
 }
 
-const store = 
-        createStore(combineReducers({heroes, filters}),
-        compose(
-            enhancer,
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-        ) 
+// const enhancer = (createStore) => (...arg) => {
+//     const store = createStore(...arg);
 
+//     const oldDispatch = store.dispatch;
+//     store.dispatch = (action) => {
+//         if (typeof action === 'string') {
+//             return oldDispatch({
+//                 type: action
+//             })
+//         }
+//         return oldDispatch(action);
+//     }
+//     return store;
+// }
+
+const store = createStore(
+                    combineReducers({heroes, filters}),
+                    compose(
+                        applyMiddleware(stringMiddleware),
+                        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+                    ); 
 
 export default store;
