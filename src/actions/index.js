091bf76f5fa33e8b063ac6220898fb4f9ad05765
RomanGrabovyi <60966944+RomanGrabovyi@users.noreply.github.com>
2022-12-22 17:16:05 +0200

    add createReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,51 +18,19 @@ export const heroesFetching = createAction('HEROES_FETCHING');
 
 export const heroesFetched = createAction('HEROES_FETCHED');
 
-export const heroesFetchingError = () => {
-    return {
-        type: 'HEROES_FETCHING_ERROR'
-    }
-}
+export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR');
 
-export const heroDeleted = (id) => {
-    return {
-        type: 'HERO_DELETED',
-        payload: id
-    }
-}
+export const heroDeleted = createAction('HERO_DELETED');
 
-export const heroCreated = (hero) => {
-    return {
-        type: 'HERO_CREATED',
-        payload: hero
-    }
-}
+export const heroCreated = createAction('HERO_CREATED');
 
-export const filtersFetching = () => {
-    return {
-        type: 'FILTER_FEATCHING'
-    }
-}
+export const filtersFetching = createAction('FILTER_FEATCHING');
 
-export const filtersFetched = (filters) => {
-    return {
-        type: 'FILTERS_FETCHED',
-        payload: filters
-    }
-}
+export const filtersFetched = createAction('FILTERS_FETCHED');
 
-export const filtersFetchingError = () => {
-    return {
-        type: 'FILTER_FETCHING_ERROR'
-    }
-}
+export const filtersFetchingError = createAction('FILTER_FETCHING_ERROR');
 
-export const activeFilterChanged = (filter) => {
-    return {
-        type: 'ACTIVE_FILTER_CHANGED',
-        payload: filter
-    }
-}
+export const activeFilterChanged = createAction('ACTIVE_FILTER_CHANGED');
 // export const activeFilterChanged = (filter) => (dispatch) => {
 //     setTimeout(() => {
 //         dispatch({
