0142fa3328a9233b38e951e541a529e3edd0bbc8
RomanGrabovyi <60966944+RomanGrabovyi@users.noreply.github.com>
2022-12-23 20:30:11 +0200

    mod heroes.js

--- src/reducers/heroes.js
+++ src/reducers/heroes.js
@@ -32,57 +32,5 @@ const heroes = createReducer(initialState, {// працює лише в нати
     },[],state => state
 )
 
-// const heroes = createReducer(initialState, builder => {
-//     builder
-//         .addCase(heroesFetching, state => {
-//             state.heroesLoadingStatus = 'loading'
-//         })
-//         .addCase(heroesFetched, (state, action) => {
-//             state.heroes = action.payload;
-//             state.heroesLoadingStatus = 'idle'
-//         })
-//         .addCase(heroesFetchingError, state => {
-//             state.heroesLoadingStatus = 'error'
-//         })
-//         .addCase(heroCreated, (state, action) => {
-//             state.heroes.push(action.payload)
-//         })
-//         .addCase(heroDeleted, (state, action) => {
-//             state.heroes = state.heroes.filter(item => item.id !== action.payload)
-//         })
-//         .addDefaultCase(() => {})
-// })
-
-// const reducer = (state = initialState, action) => {
-//     switch (action.type) {
-//         case 'HEROES_FETCHING':
-//             return {
-//                 ...state,
-//                 heroesLoadingStatus: 'loading'
-//             }
-//         case 'HEROES_FETCHED':
-//             return {
-//                 ...state,
-//                 heroes: action.payload,
-//                 heroesLoadingStatus: 'idle'
-//             }
-//         case 'HEROES_FETCHING_ERROR':
-//             return {
-//                 ...state,
-//                 heroesLoadingStatus: 'error'
-//             }
-//         case 'HERO_CREATED':
-//             return {
-//                 ...state,
-//                 heroes: [...state.heroes, action.payload]
-//             }
-//         case 'HERO_DELETED': 
-//             return {
-//                 ...state,
-//                 heroes: state.heroes.filter(item => item.id !== action.payload)
-//             }
-//         default: return state
-//     }
-// }
 
 export default heroes;
